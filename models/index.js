74255a15673cb751150c5867f9cfc4bf86d81438
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-12-27 13:43:25 +0700

    Add setup for db models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-exports.sequelize = new Sequelize(
+const modelDefiners = [require("./customer.model")];
+
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
@@ -9,3 +11,18 @@ exports.sequelize = new Sequelize(
     dialect: "postgres",
   }
 );
+
+for (const modelDefiner of modelDefiners) {
+  modelDefiner(sequelize);
+}
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Successfully synced database");
+  })
+  .catch(() => {
+    console.log("Error occured while syncing database");
+  });
+
+exports.sequelize = sequelize;
