1a852c883a36d7ee9974e38779ebeea9825ac554
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-12-31 16:33:17 +0700

    Add length validaton for dr id item

--- models/dr/id/item.model.js
+++ models/dr/id/item.model.js
@@ -5,6 +5,9 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: 1,
+      },
     },
     priceRP: {
       type: DataTypes.INTEGER,
