4e85cf706fb7056779bae5c84f264c2f51307006
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-01-02 12:58:38 +0700

    Remove delete and patch routes for discount models

--- routes/dr/discountModels.js
+++ routes/dr/discountModels.js
@@ -43,41 +43,4 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.patch("/:id", async (req, res) => {
-  try {
-    const { description, subtractor, base, percentage } = req.body;
-    const { id } = req.params;
-
-    const model = await DrDiscountModel.findByPk(id);
-
-    await model.update(
-      { description, subtractor, base, percentage },
-      {
-        where: {
-          id: id,
-        },
-      }
-    );
-
-    res.json({ data: model });
-  } catch (error) {
-    res.json({ error });
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const model = await Customer.findByPk(id);
-    await model.destroy({
-      where: {
-        id: id,
-      },
-    });
-    res.json({ data: model });
-  } catch (error) {
-    res.json({ error });
-  }
-});
-
 module.exports = router;
