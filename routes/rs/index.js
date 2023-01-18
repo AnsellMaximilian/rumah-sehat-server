68bd0a8351985faea9928b9d83421f5d5e09000b
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-01-18 16:30:42 +0700

    Add delivery router

--- routes/rs/index.js
+++ routes/rs/index.js
@@ -4,5 +4,6 @@ router.use("/suppliers", require("./suppliers"));
 router.use("/product-categories", require("./productCategories"));
 router.use("/products", require("./products"));
 router.use("/delivery-types", require("./deliveryTypes"));
+router.use("/deliveries", require("./deliveries"));
 
 module.exports = router;
