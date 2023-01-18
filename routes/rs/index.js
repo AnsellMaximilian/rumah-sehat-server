06db8db93f47378913b61ab3fb862593c806b936
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-01-18 12:50:53 +0700

    Add product product routes

--- routes/rs/index.js
+++ routes/rs/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/suppliers", require("./suppliers"));
 router.use("/product-categories", require("./productCategories"));
+router.use("/products", require("./products"));
 
 module.exports = router;
---
+++ routes/rs/products.js
@@ -0,0 +1,80 @@
+const router = require("express").Router();
+const {
+  sequelize: {
+    models: { Product },
+  },
+} = require("../../models/index");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.json({ data: products });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, price, resellerPrice, cost } = req.body;
+
+    const newProduct = Product.build({ name, price, resellerPrice, cost });
+    await newProduct.save();
+
+    res.json({ message: "Success", data: newProduct });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+    if (!product) throw `Can't find product with id ${id}`;
+    res.json({ data: product });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const { name, price, resellerPrice, cost } = req.body;
+    const { id } = req.params;
+
+    const product = await Product.findByPk(id);
+
+    await product.update(
+      { name, price, resellerPrice, cost },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    res.json({ data: product });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+    // if (product.Products.length > 0) !!!
+    //   throw "Cannot delete. Supplier has associated products.";
+    await product.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.json({ data: product });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
