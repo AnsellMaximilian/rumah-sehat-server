545b05cc092b361102de85748f47877cfd3f9ffc
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-02-22 11:52:07 +0700

    CHange date to DATEONLY attribute

--- models/rs/invoice.model.js
+++ models/rs/invoice.model.js
@@ -3,7 +3,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("Invoice", {
     date: {
-      type: DataTypes.DATE,
+      type: DataTypes.DATEONLY,
       allowNull: false,
     },
     note: {
