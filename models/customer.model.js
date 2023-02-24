64cb41146cf4c6bf4ec21aa275af1c2c161d4ac5
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-02-24 08:15:40 +0700

    Fix spelling

--- models/customer.model.js
+++ models/customer.model.js
@@ -22,7 +22,7 @@ module.exports = (sequelize) => {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     },
-    recieveDrDiscount: {
+    receiveDrDiscount: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     },
