b32a3ee3c077c672c45ffbca7b1809a1ed780dcb
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-03-10 18:13:34 +0700

    Revamp invoice and delivery creation

--- routes/rs/invoices.js
+++ routes/rs/invoices.js
@@ -53,7 +53,7 @@ router.get("/", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { CustomerId, date, note, deliveries, status } = req.body;
+    const { CustomerId, date, note, status } = req.body;
     const newInvoice = Invoice.build({
       date,
       note,
@@ -62,86 +62,104 @@ router.post("/", async (req, res, next) => {
     });
     await newInvoice.save();
 
-    for (const deliveryData of deliveries) {
-      const {
-        mode,
-        deliveryData: { date, cost, note, DeliveryTypeId, CustomerId },
-        supplierDeliveryData: { cost: supplierCost, SupplierId },
-        deliveryDetails,
-      } = deliveryData;
-
-      const delivery = Delivery.build({
-        date,
-        cost,
-        note,
-        DeliveryTypeId,
-        CustomerId,
-      });
-
-      await delivery.save();
-
-      for (const deliveryDetail of deliveryDetails) {
-        const { price, qty, ProductId, cost, designatedSaleId } =
-          deliveryDetail;
-
-        let makedetail = true;
-
-        if (designatedSaleId) {
-          const designatedSale = await PurchaseDetail.findByPk(
-            designatedSaleId
-          );
-          if (designatedSale && designatedSale.CustomerId !== null) {
-            await designatedSale.update({
-              CustomerId: null,
-            });
-          } else {
-            // designatedSale id is sent but it doesn't exist, or it does but no longer has a designated recipient
-            makedetail = false;
-          }
-        }
-
-        if (makedetail) {
-          await delivery.createDeliveryDetail({
-            price,
-            qty,
-            ProductId,
-            cost,
-          });
-        }
-      }
-
-      await newInvoice.addDelivery(delivery);
-
-      // Purchases
-
-      if (mode === "supplier") {
-        const newPurchase = Purchase.build({
-          date: date,
-          cost: supplierCost,
-          SupplierId,
-          DeliveryId: delivery.id,
-        });
-
-        await newPurchase.save();
-
-        for (const purchaseDetail of deliveryDetails) {
-          const { cost, qty, ProductId, makePurchase } = purchaseDetail;
-
-          await newPurchase.createPurchaseDetail({
-            price: cost,
-            qty,
-            ProductId,
-          });
-        }
-      }
-    }
-
     res.json({ message: "Success", data: newInvoice });
   } catch (error) {
     console.error(error);
     next(error);
   }
 });
+
+// router.post("/", async (req, res, next) => {
+//   try {
+//     const { CustomerId, date, note, deliveries, status } = req.body;
+//     const newInvoice = Invoice.build({
+//       date,
+//       note,
+//       CustomerId,
+//       status,
+//     });
+//     await newInvoice.save();
+
+//     for (const deliveryData of deliveries) {
+//       const {
+//         mode,
+//         deliveryData: { date, cost, note, DeliveryTypeId, CustomerId },
+//         supplierDeliveryData: { cost: supplierCost, SupplierId },
+//         deliveryDetails,
+//       } = deliveryData;
+
+//       const delivery = Delivery.build({
+//         date,
+//         cost,
+//         note,
+//         DeliveryTypeId,
+//         CustomerId,
+//       });
+
+//       await delivery.save();
+
+//       for (const deliveryDetail of deliveryDetails) {
+//         const { price, qty, ProductId, cost, designatedSaleId } =
+//           deliveryDetail;
+
+//         let makedetail = true;
+
+//         if (designatedSaleId) {
+//           const designatedSale = await PurchaseDetail.findByPk(
+//             designatedSaleId
+//           );
+//           if (designatedSale && designatedSale.CustomerId !== null) {
+//             await designatedSale.update({
+//               CustomerId: null,
+//             });
+//           } else {
+//             // designatedSale id is sent but it doesn't exist, or it does but no longer has a designated recipient
+//             makedetail = false;
+//           }
+//         }
+
+//         if (makedetail) {
+//           await delivery.createDeliveryDetail({
+//             price,
+//             qty,
+//             ProductId,
+//             cost,
+//           });
+//         }
+//       }
+
+//       await newInvoice.addDelivery(delivery);
+
+//       // Purchases
+
+//       if (mode === "supplier") {
+//         const newPurchase = Purchase.build({
+//           date: date,
+//           cost: supplierCost,
+//           SupplierId,
+//           DeliveryId: delivery.id,
+//         });
+
+//         await newPurchase.save();
+
+//         for (const purchaseDetail of deliveryDetails) {
+//           const { cost, qty, ProductId, makePurchase } = purchaseDetail;
+
+//           await newPurchase.createPurchaseDetail({
+//             price: cost,
+//             qty,
+//             ProductId,
+//           });
+//         }
+//       }
+//     }
+
+//     res.json({ message: "Success", data: newInvoice });
+//   } catch (error) {
+//     console.error(error);
+//     next(error);
+//   }
+// });
 
 router.patch("/:id/pay", async (req, res, next) => {
   try {
@@ -169,7 +187,7 @@ router.patch("/:id/pay", async (req, res, next) => {
 
 router.patch("/:id", async (req, res, next) => {
   try {
-    const { CustomerId, date, note, deliveries, status } = req.body;
+    const { CustomerId, date, note, status } = req.body;
 
     const { id } = req.params;
 
@@ -200,102 +218,142 @@ router.patch("/:id", async (req, res, next) => {
       }
     );
 
-    // Delete deliveries
-    await Delivery.destroy({
-      where: {
-        InvoiceId: invoice.id,
-      },
-    });
-
-    // replace deliveries
-    for (const deliveryData of deliveries) {
-      const {
-        mode,
-        editId,
-        deliveryData: { date, cost, note, DeliveryTypeId, CustomerId },
-        supplierDeliveryData: { cost: supplierCost, SupplierId },
-        deliveryDetails,
-      } = deliveryData;
-
-      const delivery = Delivery.build({
-        date,
-        cost,
-        note,
-        DeliveryTypeId,
-        CustomerId,
-      });
-
-      await delivery.save();
-
-      for (const deliveryDetail of deliveryDetails) {
-        const {
-          price,
-          qty,
-          ProductId,
-          makePurchase,
-          cost,
-          editId: detailEditId,
-          designatedSaleId,
-        } = deliveryDetail;
-
-        let makedetail = true;
-
-        if (designatedSaleId) {
-          const designatedSale = await PurchaseDetail.findByPk(
-            designatedSaleId
-          );
-          if (designatedSale && designatedSale.CustomerId !== null) {
-            await designatedSale.update({
-              CustomerId: null,
-            });
-          } else {
-            // designatedSale id is sent but it doesn't exist, or it does but no longer has a designated recipient
-            makedetail = false;
-          }
-        }
-
-        if (makedetail) {
-          await delivery.createDeliveryDetail({
-            price,
-            cost,
-            qty,
-            ProductId,
-          });
-        }
-      }
-
-      await invoice.addDelivery(delivery);
-
-      // Purchases
-
-      if (mode === "supplier" && !editId) {
-        const newPurchase = Purchase.build({
-          date: date,
-          cost: supplierCost,
-          SupplierId,
-          DeliveryId: delivery.id,
-        });
-
-        await newPurchase.save();
-
-        for (const purchaseDetail of deliveryDetails) {
-          const { cost, qty, ProductId } = purchaseDetail;
-
-          await newPurchase.createPurchaseDetail({
-            price: cost,
-            qty,
-            ProductId,
-          });
-        }
-      }
-    }
-
     res.json({ data: invoice });
   } catch (error) {
     console.log(error);
     next(error);
   }
 });
+
+// router.patch("/:id", async (req, res, next) => {
+//   try {
+//     const { CustomerId, date, note, deliveries, status } = req.body;
+
+//     const { id } = req.params;
+
+//     const invoice = await Invoice.findByPk(id, {
+//       include: [
+//         {
+//           model: Delivery,
+//           include: [
+//             { model: DeliveryType },
+//             { model: DeliveryDetail, include: Product },
+//           ],
+//         },
+//         { model: Customer },
+//       ],
+//     });
+
+//     await invoice.update(
+//       {
+//         date,
+//         note,
+//         CustomerId,
+//         status,
+//       },
+//       {
+//         where: {
+//           id: id,
+//         },
+//       }
+//     );
+
+//     // Delete deliveries
+//     await Delivery.destroy({
+//       where: {
+//         InvoiceId: invoice.id,
+//       },
+//     });
+
+//     // replace deliveries
+//     for (const deliveryData of deliveries) {
+//       const {
+//         mode,
+//         editId,
+//         deliveryData: { date, cost, note, DeliveryTypeId, CustomerId },
+//         supplierDeliveryData: { cost: supplierCost, SupplierId },
+//         deliveryDetails,
+//       } = deliveryData;
+
+//       const delivery = Delivery.build({
+//         date,
+//         cost,
+//         note,
+//         DeliveryTypeId,
+//         CustomerId,
+//       });
+
+//       await delivery.save();
+
+//       for (const deliveryDetail of deliveryDetails) {
+//         const {
+//           price,
+//           qty,
+//           ProductId,
+//           makePurchase,
+//           cost,
+//           editId: detailEditId,
+//           designatedSaleId,
+//         } = deliveryDetail;
+
+//         let makedetail = true;
+
+//         if (designatedSaleId) {
+//           const designatedSale = await PurchaseDetail.findByPk(
+//             designatedSaleId
+//           );
+//           if (designatedSale && designatedSale.CustomerId !== null) {
+//             await designatedSale.update({
+//               CustomerId: null,
+//             });
+//           } else {
+//             // designatedSale id is sent but it doesn't exist, or it does but no longer has a designated recipient
+//             makedetail = false;
+//           }
+//         }
+
+//         if (makedetail) {
+//           await delivery.createDeliveryDetail({
+//             price,
+//             cost,
+//             qty,
+//             ProductId,
+//           });
+//         }
+//       }
+
+//       await invoice.addDelivery(delivery);
+
+//       // Purchases
+
+//       if (mode === "supplier" && !editId) {
+//         const newPurchase = Purchase.build({
+//           date: date,
+//           cost: supplierCost,
+//           SupplierId,
+//           DeliveryId: delivery.id,
+//         });
+
+//         await newPurchase.save();
+
+//         for (const purchaseDetail of deliveryDetails) {
+//           const { cost, qty, ProductId } = purchaseDetail;
+
+//           await newPurchase.createPurchaseDetail({
+//             price: cost,
+//             qty,
+//             ProductId,
+//           });
+//         }
+//       }
+//     }
+
+//     res.json({ data: invoice });
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// });
 
 router.get("/:id", async (req, res, next) => {
   try {
