0e19cbd2eedaf6083ae2c1451af3fc62831ebd1f
Ansell Maximilian <ansellmaximilian@gmail.com>
2025-02-14 08:27:35 +0700

    add limit to dr id stock matches

--- routes/dr/id/items.js
+++ routes/dr/id/items.js
@@ -204,6 +204,11 @@ router.get("/:id/stock", async (req, res, next) => {
 router.get("/:id/stock-matches", async (req, res, next) => {
   try {
     const { id } = req.params;
+    const { limit } = req.query;
+    if (limit && isNaN(limit)) {
+      throw `Limit must be a number.`;
+    }
+
     const matches = await DrIdStockMatch.findAll({
       where: {
         DrIdItemId: id,
@@ -212,6 +217,7 @@ router.get("/:id/stock-matches", async (req, res, next) => {
         ["date", "DESC"],
         ["createdAt", "DESC"],
       ],
+      limit: limit ? parseInt(limit, 10) : undefined,
     });
 
     res.json({ data: matches });
